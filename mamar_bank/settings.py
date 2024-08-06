00d3c5e80200f08c772d339f930cf2c68a7020c4
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-08-06 21:50:28 +0600

    first commit

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
+# CSRF_TRUSTED_ORIGINS = ['https://django-final-exam-backend-part.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
